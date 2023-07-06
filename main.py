9cafe8086096e12b61dc9b874e1f8ac1a058dac9
mohsenamoon <dramatic225@gmail.com>
2023-07-06 19:19:56 +0330

    Completed get_references_list

--- main.py
+++ main.py
@@ -16,31 +16,29 @@ class ThesisFinder:
         text = text.decode()
         return text
 
-    def find_list_of_matches(self, pattern: str):
-        new_pattern = pattern + r'(.*[\n]*[\t]*)'
-        matches = re.findall(new_pattern, self.decoded_content)
-        matches = [re.sub(pattern, "", match) for match in matches]
+    def find_list_of_matches(self, pattern: str) -> str:
+        new_pattern = pattern + r'[\n]+[\s\S]*'
+        matches = re.findall(new_pattern, self.decoded_content)[0]
+        matches = re.sub(pattern, "", matches)
         return matches
 
     def find_matches_before_pattern(self, pattern: str) -> str:
-        new_pattern = r'.*' + pattern
+        new_pattern = r'(.*)' + pattern
         matches = re.findall(new_pattern, self.decoded_content)
-        matches = [re.sub(pattern, "", match) for match in matches]
         if not len(matches):
             return "not found"
-        return matches[0]
+        return matches[0][0]
 
     def find_matches_after_pattern(self, pattern: str) -> str:
-        new_pattern = pattern + r'.*'
+        new_pattern = pattern + r'(.*)'
         matches = re.findall(new_pattern, self.decoded_content)
-        matches = [re.sub(pattern, "", match) for match in matches]
         if not len(matches):
             return "not found"
-        return matches[0]
+        return matches[0][-1]
 
     @staticmethod
     def add_extra_space_to_patterns(patterns: list, index: str = "after") -> str:
-        extra_space = '[\n]*[\s]*'
+        extra_space = '[\n]*[^\S\r\n]*'
         joined_pattern = ''
         for pattern in patterns:
             if index == "before":
@@ -51,49 +49,61 @@ class ThesisFinder:
         return joined_pattern
 
     def get_student_name(self):
-        pattern = self.add_extra_space_to_patterns([r'دانشجو:'])
+        pattern = self.add_extra_space_to_patterns([r'(دانشجو:)'])
         name_matches = self.find_matches_after_pattern(pattern)
-        return name_matches
+        return name_matches.strip()
 
     def get_guide_name(self):
-        pattern = self.add_extra_space_to_patterns([r'استاد راهنما:'])
+        pattern = self.add_extra_space_to_patterns([r'(استاد راهنما:)'])
         name_matches = self.find_matches_after_pattern(pattern)
-        return name_matches
+        return name_matches.strip()
 
     def get_advisor_name(self):
-        pattern = self.add_extra_space_to_patterns([r'استاد مشاور:'])
+        pattern = self.add_extra_space_to_patterns([r'(استاد مشاور:)'])
         name_matches = self.find_matches_after_pattern(pattern)
-        return name_matches
+        return name_matches.strip()
 
     def get_thesis_title(self):
-        pattern = self.add_extra_space_to_patterns([r'استاد راهنما:'], "before")
+        pattern = self.add_extra_space_to_patterns([r'(استاد راهنما:)'], "before")
         name_matches = self.find_matches_before_pattern(pattern)
-        return name_matches
+        return name_matches.strip()
 
     def get_study_field(self):
-        pattern = self.add_extra_space_to_patterns([r'گروه'])
+        pattern = self.add_extra_space_to_patterns([r'(گروه)'])
         name_matches = self.find_matches_after_pattern(pattern)
-        return name_matches
+        return name_matches.strip()
 
     def get_college_name(self):
-        pattern = self.add_extra_space_to_patterns([r'دانشکده'])
+        pattern = self.add_extra_space_to_patterns([r'(دانشکده)'])
         name_matches = self.find_matches_after_pattern(pattern)
-        return name_matches
+        return name_matches.strip()
 
     def get_persian_summary(self):
-        pattern = self.add_extra_space_to_patterns([r'چکیده'])
+        pattern = self.add_extra_space_to_patterns([r'(چکیده)'])
         name_matches = self.find_matches_after_pattern(pattern)
-        return name_matches
+        return name_matches.strip()
 
     def get_english_summary(self):
-        pattern = self.add_extra_space_to_patterns([r'Abstract'])
+        pattern = self.add_extra_space_to_patterns([r'(Abstract)'])
         name_matches = self.find_matches_after_pattern(pattern)
-        return name_matches
+        return name_matches.strip()
 
-    def get_references_list(self):
-        pattern = self.add_extra_space_to_patterns([r'منابع و مآخذ'])
-        name_matches = self.find_list_of_matches(pattern)
-        return name_matches
+    def get_references_list(self) -> str:
+        matches = self.find_list_of_matches(r'منابع و مآخذ')
+        matches = "[" + "[".join(matches.split("[")[1:])
+        matches = matches.split("\n\n\n\n\n")[0]
+        matches = matches.split("Abstract")[0].split("مقاله")[0].split("واژه‌نامه")[0]
+        # new_pieces = list()
+        # pieces = matches.split("]")
+        # for num, piece in enumerate(pieces):
+        #     if num == 0:
+        #         # new_pieces.append(piece)
+        #         continue
+        #     last_piece = pieces[num - 1]
+        #     if len(re.findall(r'[0-9]+', last_piece.split("[")[-1])):
+        #         new_pieces.append(piece)
+        # new_matches = "]".join(new_pieces[:-1])
+        return matches.strip()
 
     def start_search(self):
         print(self.get_student_name(), "\n")
@@ -104,9 +114,10 @@ class ThesisFinder:
         print(self.get_college_name(), "\n")
         print(self.get_persian_summary(), "\n")
         print(self.get_english_summary(), "\n")
-        print(self.get_references_list(), "\n")
+        print("references:\n")
+        print(self.get_references_list())
 
 
 if __name__ == "__main__":
-    thesis_finder = ThesisFinder("پایان نامه شادی پورقدیری.docx")
+    thesis_finder = ThesisFinder("نمونه پایان نامه.docx")
     thesis_finder.start_search()
--- test_re.py
+++ test_re.py
@@ -1,6 +1,25 @@
 import re
+import os
 
-txt = "\n\n\n سلام سلام سلام\n\n\n\n"
+import textract
 
-matches = re.findall(r'[\n]*سلام[\n]*', txt)
+
+def process_file(file_name: str):
+    base_dir = os.getcwd()
+    text = textract.process(f'{base_dir}/{file_name}')
+    text = text.decode()
+    return text
+
+
+stack_over_flow_pattern = r'منابع و مآخذ[\n]*[^\S\r\n]*.*(?:\r?\n(?!\r?\n).*)+'
+
+txt = process_file("پایان نامه شادی پورقدیری.docx")
+matches = re.findall(r'منابع و مآخذ[\n]+[\s\S]*', txt)[0]
+# matches = re.findall(r'منابع و مآخذ[\n]*', txt)[0]
+# matches = matches.replace("منابع و مآخذ", "")
+pieces = matches.split("]")
+new_matches = "]".join(pieces[:-1])
+last_piece = pieces[-1].split("\n\n\n")[0]
+matches = "]".join([new_matches, last_piece])
+print('matches:' + matches.split("\n\n\n")[0])
 print("ok")
